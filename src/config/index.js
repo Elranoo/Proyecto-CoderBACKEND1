bb4b5ed6f3d7b230812e58154a32b6b06015b967
Thiago <thiagocedres@gmail.com>
2025-10-28 15:23:31 -0300

    .

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    PORT: process.env.PORT || 8080,
-    DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/ecommerce',
-    JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
-    // Add other configurations as needed
-};
-
-export default config;
--- src/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = require('./app');
-const PORT = 8080;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
